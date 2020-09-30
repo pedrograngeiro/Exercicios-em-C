5b9fe1e3e4fd3b5e542c606186e8cf458e04aabe
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2020-09-30 20:26:40 -0300

    Create exercicio_01.c
    Escreva um programa em C que encontre o menor inteiro positivo n que atenda as seguintes condições :
    n / 3 = x inteiro e resto 2
    n / 5 = y inteiro e resto 3
    n / 7 = z inteiro e resto 4

---
+++ lista_01/exercicio_01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+n / 3 = x inteiro e resto 2
+n / 5 = y inteiro e resto 3
+n / 7 = z inteiro e resto 4 
+*/
+
+int main()
+{
+    int i;
+    for (i = 0; ; i++)
+    {
+        if ( (i % 3 == 2) && (i % 5 == 3) && (i % 7 == 4) )
+        {
+            printf("%d", i);
+            break;
+        }
+    }
+}
